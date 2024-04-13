28133d4a67f478136bc04360762c2af556631d40
Sumit Sharma <sumitsharma7624@gmail.com>
2024-04-14 00:08:00 +0530

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+
+export const Header = () => {
+  return (
+    <div>
+      <h2 className="header-title">Expense Tracker By Sumit Sharma</h2>
+    </div>
+  );
+};
